cce2dedc0da4b8f5daac410382e477b91e1dca55
Geisy Domiciano <geisydomiciano@gmail.com>
2025-03-01 14:26:04 +0100

    :truck: :heavy_plus_sign: move views to pages and add unpluging-vue-router- plugin

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: '/', component: () => import('@/views/HomeView.vue') },
-    { path: '/projects', component: () => import('@/views/ProjectsView.vue') },
-    { path: '/projects/:id', component: () => import('@/views/SingleProjectView.vue') },
-  ],
+  routes,
 })
 
 export default router
