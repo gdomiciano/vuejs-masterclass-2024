895a91b604c1184895bde1c22754a9fa2bb8b1a9
Geisy Domiciano <geisydomiciano@gmail.com>
2025-03-02 09:17:58 +0100

    :truck::art: create layout components and reestructure sidebar links

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,14 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient';
-import { Input } from './components/ui/input';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-
-console.log(supabase)
+import TopNavBar from '@/components/Layout/TopNavBar.vue';
+import Sidebar from '@/components/Layout/Sidebar.vue';
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
-    <form action="" class="relative w-full h-fit max-w-96" role="search">
-      <iconify-icon class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
-        icon="pepicons-pop:loop"></iconify-icon>
-      <Input class="w-full pl-8 bg-background" type="search" name="search" value="" placeholder="Search" />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
-    <router-view />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavBar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
-
-<style scoped></style>
